ccafe7bb9e696fa5cd390d8f57843340b1005f67
Teru Okada <okada.teru@gmail.com>
2019-12-24 11:58:51 +0800

    first commit

---
+++ fetch_tweets.py
@@ -0,0 +1,34 @@
+
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv()
+load_dotenv(find_dotenv())
+
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("OAUTH_TOKEN")
+access_token_secret = os.getenv("OAUTH_TOKEN_SECRET")
+
+import tweepy
+
+# Search tweets.
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+tweets = tweepy.Cursor(api.search, q='地震', lang='ja').items(100)
+
+import json
+from datetime import datetime
+
+# Write tweets to json file.
+filename = 'tweets_{}.json'.format(datetime.now().strftime('%Y%m%d%H%M%S'))
+with open(filename, 'w', encoding="utf-8") as f:
+    for tweet in tweets:
+        dic = {'text': tweet.text,
+               'date': str(tweet.created_at),
+               'id': tweet.id,
+               'user_id':tweet.user.id}
+        json.dump(dic, f, ensure_ascii=False)
+        f.write('\n')
---
+++ storeInBQ.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true; do 
+ today=$(date +"%Y-%m-%d")
+ python fetch_tweets.py
+ gsutil cp *.json gs://okada_strage/tweets/$today/
+ 
+ # load コマンドを実行すると、テーブルにデータが読み込まれる
+ bq load --source_format=NEWLINE_DELIMITED_JSON   twitter.fetchFromTwitterAPITable gs://okada_strage/tweets/$today/`ls *.json`  text:STRING,date:STRING,id:INTEGER,user_id:INTEGER
+ 
+ rm *.json
+ sleep 5m
+done
