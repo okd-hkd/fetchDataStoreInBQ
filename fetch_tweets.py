e298d57051dad5bfe91cecef6ed5a31419780db9
Teru Okada <okada.teru@gmail.com>
2019-12-24 14:23:12 +0800

    added columns to schema

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,6 +1,7 @@
 
 from dotenv import load_dotenv, find_dotenv
 import os
+import math
 
 load_dotenv()
 load_dotenv(find_dotenv())
@@ -26,9 +27,16 @@ from datetime import datetime
 filename = 'tweets_{}.json'.format(datetime.now().strftime('%Y%m%d%H%M%S'))
 with open(filename, 'w', encoding="utf-8") as f:
     for tweet in tweets:
+        importance_index = round(tweet.retweet_count * 1.0 + tweet.favorite_count * 0.1)
         dic = {'text': tweet.text,
                'date': str(tweet.created_at),
                'id': tweet.id,
-               'user_id':tweet.user.id}
+               'user_id':tweet.user.id,
+              #  'created_at':tweet.created_at,
+               'retweet_count':tweet.retweet_count,
+               'favorite_count':tweet.favorite_count,
+               'importance_index': importance_index
+               }
+
         json.dump(dic, f, ensure_ascii=False)
         f.write('\n')
--- storeInBQ.sh
+++ storeInBQ.sh
@@ -5,7 +5,7 @@ while true; do
  gsutil cp *.json gs://okada_strage/tweets/$today/
  
  # load コマンドを実行すると、テーブルにデータが読み込まれる
- bq load --source_format=NEWLINE_DELIMITED_JSON   twitter.fetchFromTwitterAPITable gs://okada_strage/tweets/$today/`ls *.json`  text:STRING,date:STRING,id:INTEGER,user_id:INTEGER
+ bq load --source_format=NEWLINE_DELIMITED_JSON twitter.fetchFromTwitterAPITable gs://okada_strage/tweets/$today/`ls *.json` text:STRING,date:STRING,id:INTEGER,user_id:INTEGER,importance_index:INTEGER,retweet_count:INTEGER,favorite_count:INTEGER
  
  rm *.json
  sleep 5m
